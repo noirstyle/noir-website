0b56829bd95ed8cfc10c0b3f353b29b2f7915556
noirstyle <noirstyle.tr@gmail.com>
2026-07-14 01:53:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("NOIR loaded");
+// =========================
+// SCROLL ARROW HIDE
+// =========================
+
+const arrow = document.querySelector(".scroll-arrow");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 100) {
+        arrow.style.opacity = "0";
+    } else {
+        arrow.style.opacity = "1";
+    }
+
+});
